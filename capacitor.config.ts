8375a1272bd79a6079d393ececec36ab89759828
cxq255zzfr-sudo <guventurkdanismanlik@gmail.com>
2026-04-14 23:20:24 +0300

    fix: keep android app name in english

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.breakcash.app',
-  appName: 'Break Cash',
+  appName: 'BreakCash',
   webDir: 'dist',
   bundledWebRuntime: false,
   server: {
